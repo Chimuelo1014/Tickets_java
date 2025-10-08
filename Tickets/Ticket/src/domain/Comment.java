eca55cc2cefec169b3dbad68d60614a29ea03a53
Samuel Quintero <quinterosanchezsamuel1@gmail.com>
2025-10-07 21:18:48 -0500

    Delete Tickets directory

--- Tickets/Ticket/src/domain/Comment.java
+++
@@ -1,8 +0,0 @@
-package domain;
-// import java.util.Date;
-public class Comment {
-    private int id;
-    private String date;
-    private String text;
-    private int  ticket_id; // esta seria foreign key
-}
